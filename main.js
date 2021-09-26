ab4c43c5ff1af4a0ff2baa2bb1faac9caa8c220b
‘gcd’ <it_gcd@163.com>
2021-09-26 21:35:31 +0800

    修复鼠标篡位

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 let box=document.getElementById("box");
 //当鼠标在被拖拽元素按下时，开始拖拽    onmousedown
-box.onmousedown= function (){
+box.onmousedown= function (event){
+        //获取div偏移量  
+        let ol = event.clientX - box.offsetLeft;
+        let ot = event.clientY - box.offsetTop;
     //鼠标跟随移动时拖拽元素跟着移动       onmousemove
     document.onmousemove =  function(event){
         event=event || window.event;
-        //获取鼠标坐位
-        let left = event.clientX;
-        let top = event.clientY;
-        //修改位置
+        //获取鼠标的坐标
+        let left = event.clientX - ol;
+        let top = event.clientY - ot;
+        //修改box位置
         box.style.left = left + "px";
         box.style.top = top + "px";
     }
