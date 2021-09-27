76edf74130241273f30e3eb4d97d4ffeca6220a6
‘gcd’ <it_gcd@163.com>
2021-09-27 22:34:15 +0800

    捕获释放

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 let box=document.getElementById("box");
 //当鼠标在被拖拽元素按下时，开始拖拽    onmousedown
 box.onmousedown= function (event){
+        //因为超出窗口还能跟随移动，so需捕捉释放鼠标
+        //捕捉
+        //if(box.setCapture) box.setCapture();
+        box.setCapture && box.setCapture()
         //获取div偏移量  
         let ol = event.clientX - box.offsetLeft;
         let ot = event.clientY - box.offsetTop;
@@ -13,10 +17,14 @@ box.onmousedown= function (event){
         //修改box位置
         box.style.left = left + "px";
         box.style.top = top + "px";
-    }
+    };
+    //鼠标松开时，该元素固定到当前位置     onmouseup
+        box.onmouseup = function (){
+            document.onmousemove = null;
+            document.onmouseup = null;
+             //释放，取消对事件的捕获 
+             box.releaseCapture && box.releaseCapture();
+        };
+        //拖拽内容时，浏览器会搜索引擎搜索，此时会导致拖拽功能异常     取消
+        return false;                                                    
 };
-//鼠标松开时，该元素固定到当前位置     onmouseup
-box.onmouseup = function (){
-    //释放，取消onmouseve事件
-    document.onmousemove = null;
-}
